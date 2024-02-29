b0a566f8611a165e29097b277e2d46696e6660db
LEONARDO TORQUATO DE ANDRADE <23111738@dsn127m1246730.cfp127aluno.local>
2024-02-29 11:07:57 -0300

    Finalizado

--- app/src/main/java/br/senai/sp/jandira/calculadoraimc/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/calculadoraimc/MainActivity.kt
@@ -5,28 +5,32 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -54,47 +58,154 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting() {
     Column(
-        modifier = Modifier
-            .background(Color.Red)
-            .height(250.dp)
-    ){
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(100.dp)
-                .padding(8.dp),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.Bottom
-        ){
-            Card(modifier = Modifier
-                .width(70.dp)
-                .height(70.dp),
-                shape = CircleShape
-            ) {
-            Image(
-                painter = painterResource(id = R.drawable.bmi),
-                contentDescription = "Bmi image",
-                contentScale = ContentScale.Crop)
-
-            }
-        }
+        modifier = Modifier.fillMaxHeight()
+    ) {
         Column(
             modifier = Modifier
-                .height(50.dp)
-                .padding(start = 125.dp),
-            verticalArrangement = Arrangement.Center
-        ) {
+                .fillMaxWidth()
+                .height(200.dp)
+                .background(Color(0xFFE61753)),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Image(painterResource(
+                id = R.drawable.bmi),
+                contentDescription = "",
+                modifier = Modifier
+                    .width(70.dp)
+                    .height(100.dp)
+            )
             Text(
                 text = "Calculadora IMC",
-                fontSize = 18.sp,
                 color = Color.White,
-                fontWeight = FontWeight.Bold,
+                fontSize = 28.sp
 
             )
+        }
+        Card(
+            modifier = Modifier
+                .width(350.dp)
+                .height(400.dp)
+                .padding(start = 30.dp)
+                .offset(y = -50.dp),
+            colors = CardDefaults.cardColors(Color(0xfff2f5f7)),
+            elevation = CardDefaults.cardElevation(defaultElevation = 12.dp)
+        )
+        {
+            Column(
+                modifier = Modifier
+                    .padding(start = 20.dp, top = 20.dp)
+                    .fillMaxHeight()
+            ) {
+
+                Text(
+                    text = "Seus Dados",
+                    fontSize = 28.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFFE61753),
+                    modifier = Modifier.padding(start = 50.dp)
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+                Text(
+                    text = "Seu Peso:",
+                    color = Color(0xFFE61753)
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    placeholder = {
+                        Text(text = "Seu Peso em kg")
+                    },
+                    colors = OutlinedTextFieldDefaults
+                        .colors(
+                            unfocusedContainerColor = Color.White,
+                            focusedContainerColor = Color.White,
+                            unfocusedBorderColor = Color(0xFFE61753),
+                            focusedBorderColor = Color(0xFFE61753),
+                            unfocusedPlaceholderColor = Color.Gray,
+                        )
+
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+                Text(
+                    text = "Sua Altura:",
+                    color = Color(0xFFE61753)
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    placeholder = {
+                        Text(text = "Sua Altura em cm")
+                    },
+                    colors = OutlinedTextFieldDefaults
+                        .colors(
+                            unfocusedContainerColor = Color.White,
+                            focusedContainerColor = Color.White,
+                            unfocusedBorderColor = Color(0xFFE61753),
+                            focusedBorderColor = Color(0xFFE61753),
+                            unfocusedPlaceholderColor = Color.Gray,
+                        )
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color(0xFFE61753)),
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier
+                        .width(280.dp)
+
+
+                ) {
+                    Text(
+                        text = "CALCULAR",
+                        fontSize = 18.sp,
+                        letterSpacing = 1.sp
+                    )
+
+                }
+
+            }
 
         }
-    }
+        Card(
+            modifier = Modifier
+                .width(350.dp)
+                .height(100.dp)
+                .padding(start = 30.dp),
+            colors = CardDefaults.cardColors(Color(0xff329F6B)),
+            elevation = CardDefaults.cardElevation(defaultElevation = 16.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(top = 20.dp, start = 40.dp)
 
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.padding(5.dp)
+                ) {
+                    Text(
+                        text = "Resultado",
+                        color = Color.White
+                    )
+                    Text(
+                        text = "Peso Ideal",
+                        fontSize = 20.sp,
+                        color = Color.White
+                    )
+                }
+                Spacer(modifier = Modifier.width(40.dp))
+                Text(
+                    text = "21.3",
+                    fontSize = 36.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
